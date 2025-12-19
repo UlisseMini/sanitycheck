b819c79ac63ffcf96cbdf120be24939d01fcbff7
Ulisse Mini <ulisse.mini@gmail.com>
2025-12-18 22:15:06 -0800

    Move tests from __tests__ to tests/

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['__tests__/**/*.test.ts', 'src/**/*.test.ts'],
+    include: ['tests/**/*.test.ts', 'src/**/*.test.ts'],
     globals: true,
   },
 });
